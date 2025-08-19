3835ca0fd6757cc9d02eac6525c97be94b969d05
Ravi Lachhman <ravi@lachhman.io>
2025-08-19 11:33:58 -0400

    Removed old files

--- backend/src/main/java/com/wizkhalubernetes/repository/QuoteJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wizkhalubernetes.repository;
-
-import com.wizkhalubernetes.model.Quote;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuoteJpaRepository extends JpaRepository<Quote, Long> {
-}
--- backend/src/main/java/com/wizkhalubernetes/repository/QuoteMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wizkhalubernetes.repository;
-
-import com.wizkhalubernetes.model.Quote;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface QuoteMongoRepository extends MongoRepository<Quote, Long> {
-}
